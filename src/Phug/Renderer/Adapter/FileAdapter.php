13acb84ecb7af164e2d1f6985db01fc1f38887a3
KyleKatarn <kylekatarnls@gmail.com>
2018-07-05 14:34:42 +0200

    Merge events only in the file adater for cacheDirectory

--- src/Phug/Renderer/Adapter/FileAdapter.php
+++ src/Phug/Renderer/Adapter/FileAdapter.php
@@ -154,10 +154,12 @@ class FileAdapter extends AbstractAdapter implements CacheInterface
         $errorDetails = [];
 
         $renderer = $this->getRenderer();
+        $events = $renderer->getCompiler()->getEventListeners();
 
         foreach ($renderer->scanDirectory($directory) as $inputFile) {
             $renderer->initCompiler();
             $compiler = $renderer->getCompiler();
+            $compiler->mergeEventListeners($events);
             $path = $inputFile;
             $this->isCacheUpToDate($path);
             $sandBox = $this->getRenderer()->getNewSandBox(function () use (&$success, $compiler, $path, $inputFile) {
